b712bb50eced74fbe32782fbbe807374d7eb9ce2
prem310 <prem_tech@yahoo.com>
2016-01-07 00:45:41 -0500

    Delete sampleDirective.js

--- directive/sampleDirective.js
+++
@@ -1,8 +0,0 @@
-var directiveModule=angular.module("directiveModule",[]);
-
-directiveModule.directive("columnar",function(){
-	return {
-		restrict:"E",
-		template:"<div>This is working</div>"
-	}
-});
